982bd5d22aab67e4fe89b324a9e1936cff9db3a7
Sergey Vinogradov <mr.vursen@gmail.com>
2025-03-19 11:46:34 +0400

    wip

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,41 +1,32 @@
 package org.vaadin.example;
 
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route
+@Route("")
 public class MainView extends Div {
+    private static final ValueChangeListener<ComponentValueChangeEvent<Select<String>, String>> NO_OP = (event) -> {
+    };
 
     public MainView() {
+        getStyle().setPadding("2em");
+
+        /* Field palette */
+        // TextField firstName = new TextField("First name");
+        // TextField lastName = new TextField("Last name");
+        // Select<String> country = new Select<>("Country", NO_OP, "Finland");
+        // Select<String> city = new Select<>("City", NO_OP, "Helsinki", "Turku");
+        // TextField zipCode = new TextField("ZIP code");
+        // TextArea streetAddress = new TextArea("Street address");
+
+        /* Form Layout */
         FormLayout formLayout = new FormLayout();
-        formLayout.setAutoResponsive(true);
-
-        TextField firstName = new TextField("First name");
-        TextField lastName = new TextField("Last name");
-
-        Select<String> gender = new Select<>();
-        gender.setLabel("Gender");
-        gender.setItems("Male", "Female", "Other");
-
-        EmailField email = new EmailField("Email");
-
-        TextField address = new TextField("Address");
-
-        Select<String> country = new Select<>();
-        country.setLabel("Country");
-        country.setItems("Finland");
-
-        Select<String> city = new Select<>();
-        city.setLabel("City");
-        city.setItems("Helsinki", "Turku", "Tampere", "Oulu");
-
-        formLayout.add(firstName, lastName, email, gender, address, country, city);
-
         add(formLayout);
     }
 }
